31b0b21308d162e265eea569a8e1c3f3060b7292
Azil6744 <developmentwithazil@gmail.com>
2025-05-26 15:03:12 +0500

    Fix class name casing for Subsubserver in EditSubSubServer component

--- app/Livewire/Admin/EditSubSubServer.php
+++ app/Livewire/Admin/EditSubSubServer.php
@@ -5,7 +5,7 @@ namespace App\Livewire\Admin;
 use App\Models\SubServer;
 use App\Models\VpsServer;
 use Livewire\Component;
-use App\Models\SubsubServer;
+use App\Models\Subsubserver;
 
 class EditSubSubServer extends Component
 {
