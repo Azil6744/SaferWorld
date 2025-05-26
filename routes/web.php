ba52a87771b08fc0ef3d38537b94182240869474
Azil6744 <developmentwithazil@gmail.com>
2025-05-26 14:51:55 +0500

    Add Artisan command routes for migration, seeding, and optimization

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,32 @@ require __DIR__ . '/admin.php';
 
 Route::get('/test', function () {
     return view('auth.verify');
+});
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return "Migrated";
+});
+Route::get('/rollback-migration', function () {
+    Artisan::call('migrate:rollback --step=1');
+    return "Rolled back migration";
+});
+
+Route::get('/seed', function () {
+    Artisan::call('db:seed');
+    return "Seeded";
+});
+
+Route::get('/optimize-clear', function () {
+    Artisan::call('optimize:clear');
+    return "Cleared";
+});
+
+Route::get('/optimize', function () {
+    Artisan::call('optimize');
+    return "Optimized";
+});
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return "Linked";
 });
